ae0f5bb3c10ec020ff5d5c77bce4b45534175630
Samuel Groß <dev@samuel-gross.de>
2013-11-11 14:49:25 +0100

    Added cycle detection function: has_cycles

--- basics.py
+++ basics.py
@@ -43,3 +43,46 @@ def _dfs(graph, cur, t, path):
                 
     # done with this node    
     return False
+
+
+
+def has_cycles(graph):
+    """
+    Returns a list of path objects, each making up a cycle in the provided graph.
+
+    If there is no path and empty list will be returned.
+    """
+    if graph.empty():
+        return []
+
+    # initialization
+    for node in graph.nodes():
+        node.color = "white"
+
+    # dfs from the first node
+    res = _dfs_cycle(graph, graph.nodes()[0], Path(graph.nodes()[0]))
+
+    # cleanup
+    for node in graph.nodes():
+        del node.color
+
+    return res
+
+
+def _dfs_cycle(graph, cur, path):
+    cur.color = "grey"
+    for edge in cur.outgoing_edges():
+        n = edge.destination()
+        if n.color == "white":
+            path.append(edge, n)
+            if _dfs_cycle(graph, n, path):
+                return True
+            # continue with next neighbor
+            path.pop()
+        elif n.color == "grey":
+            # cycle detected
+            return True
+
+    # done with this node    
+    cur.color = "black"
+    return False
