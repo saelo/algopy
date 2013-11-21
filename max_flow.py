2738b8eac04fe8ce480f85e93cc064e2601f6a64
Samuel Groß <dev@samuel-gross.de>
2013-11-21 22:35:28 +0100

    Minor code improvements

--- max_flow.py
+++ max_flow.py
@@ -11,13 +11,13 @@ from graph import *
 
 def _get_active_node(graph, s, t):
     for node in graph.nodes():
-        if not node == t and not node == s and node.overrun > 0:
+        if not node is t and not node is s and node.overrun > 0:
             return node
 
     return None
 
 def _has_active_node(graph, s, t):
-    return True if not _get_active_node(graph, s, t) == None else False
+    return True if not _get_active_node(graph, s, t) is None else False
 
 def _push(node):
     """
@@ -41,7 +41,7 @@ def _push(node):
         neighbor.overrun  += push
         node.overrun      -= push
 
-        print("pushing %i from %s to %s" % (push, node.name(), neighbor.name()))
+        print("[*] pushing %i from %s to %s" % (push, node.name(), neighbor.name()))
 
         if node.overrun == 0:
             break
@@ -59,7 +59,7 @@ def _relabel(node):
     for edge in node.outgoing_edges():
         if edge.load == edge.capacity:
             continue
-        if min_dist == None or edge.destination().dist < min_dist:
+        if min_dist is None or edge.destination().dist < min_dist:
             min_dist = edge.destination().dist
 
     node.dist = min_dist + 1
@@ -96,7 +96,7 @@ def solve_max_flow(graph, s, t):
         node = _get_active_node(graph, s, t)
         if not _push(node):
             _relabel(node)
-            print("relabeling %s to dist %i" % (node.name(), node.dist))
+            print("[*] relabeling %s to dist %i" % (node.name(), node.dist))
 
     # cleanup
     for edge in graph.edges():
